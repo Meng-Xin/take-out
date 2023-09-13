0ce33dd0a9f96e21fb7a87bb07f3b3936ebb99db
Meng-Xin <li997266754@gmail.com>
2023-09-13 16:01:00 +0800

    feat: 实现员工修改密码

--- internal/api/request/employee.go
+++ internal/api/request/employee.go
@@ -4,3 +4,18 @@ type EmployeeLogin struct {
 	UserName string `json:"username" binding:"required"`
 	Password string `json:"password"  binding:"required"`
 }
+
+type EmployeeEditPassword struct {
+	EmpId       uint64 `json:"empId"`
+	NewPassword string `json:"newPassword" binding:"required"`
+	OldPassword string `json:"oldPassword" binding:"required"`
+}
+
+type EmployeeDTO struct {
+	Id       uint64 `json:"id"`                          //员工id
+	IdNumber string `json:"idNumber" binding:"required"` //身份证
+	Name     string `json:"name" binding:"required"`     //姓名
+	Phone    string `json:"phone" binding:"required"`    //手机号
+	Sex      string `json:"sex" binding:"required"`      //性别
+	UserName string `json:"username" binding:"required"` //用户名
+}
