d691106afd167f78f7b0583a090ad46738be19eb
Meng-Xin <li997266754@gmail.com>
2023-09-13 18:44:52 +0800

    feat: 新增员工分页查询dao层实现

--- internal/repository/dao/employee_dao.go
+++ internal/repository/dao/employee_dao.go
@@ -3,12 +3,34 @@ package dao
 import (
 	"context"
 	"gorm.io/gorm"
+	"take-out/common"
+	"take-out/internal/api/request"
 	"take-out/internal/model"
 	"take-out/internal/repository"
 )
 
 type EmployeeDao struct {
 	db *gorm.DB
+}
+
+func (e *EmployeeDao) PageQuery(ctx context.Context, dto request.EmployeePageQueryDTO) (*common.PageResult, error) {
+	// 分页查询 select count(*) from employee where name = ? limit x,y
+	var result common.PageResult
+	var employeeList []model.Employee
+	var err error
+	// 动态拼接
+	query := e.db.WithContext(ctx).Model(&model.Employee{})
+	if dto.Name != "" {
+		query = query.Where("name LIKE ?", "%"+dto.Name+"%")
+	}
+	// 计算总数
+	if err = query.Count(&result.Total).Error; err != nil {
+		return nil, err
+	}
+	// 分页查询
+	err = query.Scopes(result.Paginate(&dto.Page, &dto.PageSize)).Find(&employeeList).Error
+	result.Records = employeeList
+	return &result, err
 }
 
 func (e *EmployeeDao) Insert(ctx context.Context, entity model.Employee) error {
