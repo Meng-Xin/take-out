f8e04275d36e27291e462b42b2b02f17dfb19056
Meng-Xin <li997266754@gmail.com>
2023-09-17 21:03:58 +0800

    feat：新增dishRouter和CommonRouter

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,6 +5,8 @@ import "take-out/internal/router/admin"
 type RouterGroup struct {
 	admin.EmployeeRouter
 	admin.CategoryRouter
+	admin.DishRouter
+	admin.CommonRouter
 }
 
 var AllRouter = new(RouterGroup)
