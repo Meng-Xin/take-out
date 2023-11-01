55578e42d96f5669789f5d09e294b802031f5619
Meng-Xin <li997266754@gmail.com>
2023-11-01 21:01:32 +0800

    feat:  setmeal(套餐模块) 增添SetMealPageQueryVo结构体

---
+++ internal/api/response/setmeal.go
@@ -0,0 +1,18 @@
+package response
+
+import "time"
+
+type SetMealPageQueryVo struct {
+	Id           uint64    `json:"id" `          // 主键id
+	CategoryId   uint64    `json:"categoryId"`   // 分类id
+	Name         string    `json:"name"`         // 套餐名称
+	Price        float64   `json:"price"`        // 套餐单价
+	Status       int       `json:"status"`       // 套餐状态
+	Description  string    `json:"description"`  // 套餐描述
+	Image        string    `json:"image"`        // 套餐图片
+	CreateTime   time.Time `json:"createTime"`   // 创建时间
+	UpdateTime   time.Time `json:"updateTime"`   // 更新时间
+	CreateUser   uint64    `json:"createUser"`   // 创建用户
+	UpdateUser   uint64    `json:"updateUser"`   // 更新用户
+	CategoryName string    `json:"categoryName"` // 分类名称
+}
