1ea70ec542d19f1d02e10a69102bc1d96de84830
Meng-Xin <li997266754@gmail.com>
2023-11-01 18:45:21 +0800

    fix:  setmeal(套餐模块) 修復错误的数据格式不匹配无法解析。 ErrReason：前段传递的Price为string类型，服务端接受为Float64。

--- internal/api/request/setmeal.go
+++ internal/api/request/setmeal.go
@@ -6,7 +6,7 @@ type SetMealDTO struct {
 	Id           uint64              `json:"id"`            // 主键id
 	CategoryId   uint64              `json:"categoryId"`    // 分类id
 	Name         string              `json:"name"`          // 套餐名称
-	Price        float64             `json:"price"`         // 套餐单价
+	Price        string              `json:"price"`         // 套餐单价
 	Status       int                 `json:"status"`        // 套餐状态
 	Description  string              `json:"description"`   // 套餐描述
 	Image        string              `json:"image"`         // 套餐图片
