18f64d75ae630e85bb863bacf39d6eec714cf21f
Meng-Xin <li997266754@gmail.com>
2023-11-01 18:25:29 +0800

    feat: 增添错误下发函数，简洁化Controller层的错误消息发送流程。

--- common/e/msg.go
+++ common/e/msg.go
@@ -1,5 +1,11 @@
 package e
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"take-out/common"
+)
+
 var ErrMsg = map[int]string{
 	SUCCESS:         "ok",
 	ERROR:           "内部错误",
@@ -8,4 +14,8 @@ var ErrMsg = map[int]string{
 
 func GetMsg(code int) string {
 	return ErrMsg[code]
+}
+
+func Send(ctx *gin.Context, code int) {
+	ctx.JSON(http.StatusOK, common.Result{Code: code, Msg: GetMsg(code)})
 }
