6a92d164673ad88520c860b6cc8cd3cd6acd1489
Meng-Xin <li997266754@gmail.com>
2023-11-01 18:27:18 +0800

    feat: 新增套餐模块 路由结构初始化代码

---
+++ internal/router/admin/setmeal.go
@@ -0,0 +1,26 @@
+package admin
+
+import (
+	"github.com/gin-gonic/gin"
+	"take-out/global"
+	"take-out/internal/api/controller"
+	"take-out/internal/repository/dao"
+	"take-out/internal/service"
+	"take-out/middle"
+)
+
+type SetMealRouter struct{}
+
+func (sr *SetMealRouter) InitApiRouter(parent *gin.RouterGroup) {
+	//publicRouter := parent.Group("category")
+	privateRouter := parent.Group("setmeal")
+	// 私有路由使用jwt验证
+	privateRouter.Use(middle.VerifyJWTAdmin())
+	// 依赖注入
+	setmealCtrl := controller.NewSetMealController(
+		service.NewSetMealService(dao.NewSetMealDao(global.DB), dao.NewSetMealDishDao()),
+	)
+	{
+		privateRouter.POST("", setmealCtrl.SaveWithDish)
+	}
+}
