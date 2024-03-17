fc0ea7f0ec125a3f778144d14fd3501960f8a108
Meng-Xin <li997266754@gmail.com>
2024-03-17 11:58:09 +0800

    fix: 更换 SetMealDishDao 成员方法的gorm.DB入参。
    使用工具方法获取transaction中的db

--- internal/repository/dao/setmeal_dish_dao.go
+++ internal/repository/dao/setmeal_dish_dao.go
@@ -1,7 +1,7 @@
 package dao
 
 import (
-	"gorm.io/gorm"
+	"take-out/global/tx"
 	"take-out/internal/model"
 	"take-out/internal/repository"
 )
@@ -9,14 +9,22 @@ import (
 type SetMealDishDao struct {
 }
 
-func (s SetMealDishDao) GetBySetMealId(transaction *gorm.DB, SetMealId uint64) ([]model.SetMealDish, error) {
+func (s SetMealDishDao) GetBySetMealId(transactions tx.Transaction, SetMealId uint64) ([]model.SetMealDish, error) {
 	var dishList []model.SetMealDish
-	err := transaction.Where("setmeal_id = ?", SetMealId).Find(&dishList).Error
+	db, err := tx.GetGormDB(transactions)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Where("setmeal_id = ?", SetMealId).Find(&dishList).Error
 	return dishList, err
 }
 
-func (s SetMealDishDao) InsertBatch(transaction *gorm.DB, setmealDishs []model.SetMealDish) error {
-	err := transaction.Create(&setmealDishs).Error
+func (s SetMealDishDao) InsertBatch(transactions tx.Transaction, setmealDishs []model.SetMealDish) error {
+	db, err := tx.GetGormDB(transactions)
+	if err != nil {
+		return err
+	}
+	err = db.Create(&setmealDishs).Error
 	return err
 }
 
